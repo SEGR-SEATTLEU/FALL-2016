9b169ce66a9be601a894579c516ab38f50b09eef
scottshipp <shipps@seattleu.edu>
2016-10-21 21:27:19 -0700

    Updated sql. Dates no longer hardcoded. Started manage page US-08.

--- expressapp/routes/index.js
+++ expressapp/routes/index.js
@@ -16,11 +16,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/returns', function(req, res, next) {
+  var dueDate = req.query.duedate;
+  var qry = `CALL RequestsDueForReturn(\'${dueDate}\');`;
+  connection.query(qry, function(err, rows, fields) {
+    if (err) res.render('index', { title: 'error!' });
+    res.render('managereturns', { results: rows[0] });
+   }); 
+});
+
 router.get('/available', function(req, res, next) {
-  connection.query('CALL AvailableItems(\'2016-11-04\', \'2016-11-08\');', function(err, rows, fields) {
+  var pickupDate = req.query.pickupdate;
+  var returnDate = req.query.returndate;
+  var qry = `CALL AvailableItems(\'${pickupDate}\', \'${returnDate}\');`;
+  connection.query(qry, function(err, rows, fields) {
     if (err) res.render('index', { title: 'error!' });
     res.render('availableitems', { results: rows[0] });
   });
 });
+
+
 
 module.exports = router;

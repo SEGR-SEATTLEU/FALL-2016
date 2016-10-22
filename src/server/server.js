3b2f39b2785bf140605fff86c61d523bf3598b44
hisham02 <hisham02@gmail.com>
2016-10-22 15:47:30 -0700

    Added body-parser npm
    Re-added body parser npm package to help with JSON parsing. body-parser
    extracts the entire body portion of an incoming request stream and
    exposes it on req.body as something easier to interface with.

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var connection = require('./config/db-connection');
+var bodyparser = require('body-parser')
 var gearRequestController = require('./controllers/gear-request-controller');
 var helloWorldController = require('./controllers/hello-world-controller');
 var app = express();
+
+
+// Configured app with body parser for accepting POST requests.
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());  
 
 connection.init();
 gearRequestController.configure(app);

32289cd6a038b9a6517fa5c0ae919c270a615c1c
hisham02 <hisham02@gmail.com>
2016-10-26 02:01:36 -0700

    Re-implemented creation of gear request. JSON object is being passed to MySQL

--- src/server/services/gear-request-service.js
+++ src/server/services/gear-request-service.js
@@ -13,27 +13,13 @@ function Request() {
     // Creating Request and reserving the gears for the request.
     this.post = function (req, res) {
         connection.acquire(function (err, con) {
-            con.query('CALL CreateRequest(\'' + req.startdate + '\',\'' + req.enddate + '\', 1);', req, function (err, result) {
+            con.query('CALL CreateRequest(\'' + req.startdate + '\',\'' + req.enddate + '\',\'' + JSON.stringify(req) + '\',1);', req, function (err, result) {
                 con.release();    
                 if (err) {
                     console.log(err);
                     throw err;
                 } else {
-                    console.log(result);
-                    connection.acquire(function (err, con) {
-                        for (var i = 0; i < req.gears.length; i++) {
-                            con.query('CALL ReserveItem(' + result[0][0].RequestID + ',' + req.gears[i].quantity + ',' + req.gears[i].id + ');', req.gears, function (err, result) {
-                                if (err) {
-                                    console.log(err);
-                                    throw err;
-                                } else {
-                                    res.send("Added all gears to the request");
-                                    console.log(result);
-                                }
-                            });
-                        }
-                    });
-                    con.release();                   
+                    console.log(result);                 
                 }
             });    
         });

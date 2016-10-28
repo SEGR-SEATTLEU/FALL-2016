a07ba3dd17ed87c5272768da2928e0ccc53551a3
hisham02 <hisham02@gmail.com>
2016-10-28 01:44:49 -0700

    Fixed some issues and re-uploaded US-03 SQL code and NodeJS post function

--- src/server/services/gear-request-service.js
+++ src/server/services/gear-request-service.js
@@ -3,6 +3,8 @@ var connection = require('../config/db-connection');
 function Request() {
 
     // Getting request details
+    // pre:
+    // post: 
     this.get = function (res) {
         connection.acquire(function (err, con) {
             con.query('', function (err, result) {
@@ -11,15 +13,18 @@ function Request() {
     };
 
     // Creating Request and reserving the gears for the request.
+    // pre: request params include startdate, enddate, JSON containing gears, UserID.
+    // post: error is logged if request is not created successfully.
     this.post = function (req, res) {
         connection.acquire(function (err, con) {
-            con.query('CALL CreateRequest(\'' + req.startdate + '\',\'' + req.enddate + '\',\'' + JSON.stringify(req) + '\',1);', req, function (err, result) {
+            con.query('CALL create_request(\'' + req.startdate + '\',\'' + req.enddate + '\',\'' + JSON.stringify(req) + '\',1);', req, function (err, result) {
                 con.release();    
                 if (err) {
                     console.log(err);
                     throw err;
                 } else {
-                    console.log(result);                 
+                    console.log(result);
+                    res.send(result);
                 }
             });    
         });

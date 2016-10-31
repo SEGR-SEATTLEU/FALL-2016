8d48e61b409af678923ce7e9969cc0e469f53083
scottashipp <shipps@seattleu.edu>
2016-10-31 15:45:29 -0700

    Restore original gear-service file too

--- src/server/services/gear-service.js
+++ src/server/services/gear-service.js
@@ -4,17 +4,8 @@ var connection = require('../config/db-connection');
 function Gear() {
 
     this.get = function (req, res) {
-
-        var startdate = req.query.startdate;
-        var enddate = req.query.enddate;
-        console.log('received startdate of ' + startdate);
-        console.log('received enddate of ' + enddate);
-
         connection.acquire(function (err, con) {
-            if(typeof con.query == 'undefined') {
-              console.log('Bazunda!!');
-            }
-            con.query('CALL Gear_Availability(\'' + startdate + '\',\'' + enddate + '\')', req, function (err, result) {
+            con.query('CALL Gear_Availability(\'' + req.query.startdate + '\',\'' + req.query.enddate + '\')', req, function (err, result) {
                 con.release();
                 if (err) {
                     console.log(err);

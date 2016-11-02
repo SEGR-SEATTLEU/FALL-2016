0518736371b0134c609869ccc9d1b2d8a9869a56
scottashipp <shipps@seattleu.edu>
2016-11-02 12:27:28 -0700

    Update delimiter statements and begin end so US-08 sproc creation is not skipped

--- SQL/SQL_Database_Setup.sql
+++ SQL/SQL_Database_Setup.sql
@@ -549,6 +549,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS RequestsDueForReturn;
 DELIMITER $$
 CREATE PROCEDURE RequestsDueForReturn() READS SQL DATA
+BEGIN
 	SELECT rq.id request_id, rq.end_date due_date, st.`status`, cr.`name` borrower, cr.email, 
 			gr.gender, sz.size, gi.`name`, ri.quantity
 		FROM request rq
@@ -558,20 +559,31 @@ CREATE PROCEDURE RequestsDueForReturn() READS SQL DATA
         JOIN gear_item gi ON ri.item_id = gi.id
         JOIN size sz ON gi.size_id = sz.id JOIN gender gr on gi.gender_id = gr.id
         WHERE st.id = 3;
-        
+END$$
+
 DELIMITER ;        
 DROP PROCEDURE IF EXISTS SetRequestStatus;
 DELIMITER $$
 CREATE PROCEDURE SetRequestStatus(IN `RequestId` INT, IN `Status` INT)
+BEGIN
 	UPDATE request set status_id=`Status` WHERE request.id = RequestId;
+END$$
+
 DELIMITER ;        
 DROP PROCEDURE IF EXISTS MarkRequestReturned;
-CREATE PROCEDURE MarkRequestReturned(IN `RequestId` INT)
-	UPDATE request set status_id=4 WHERE request.id = RequestId;
+
 DELIMITER $$
+CREATE PROCEDURE MarkRequestReturned(IN `RequestId` INT)
+BEGIN
+	UPDATE request set status_id=4 WHERE request.id = RequestId;
+END$$
+
 DELIMITER ;            
 DROP PROCEDURE IF EXISTS ViewRequestDetail;
+
+DELIMITER $$
 CREATE PROCEDURE ViewRequestDetail(IN `RequestId` INT) READS SQL DATA
+BEGIN
 	SELECT rq.id request_id, rq.end_date due_date, st.`status`, cr.`name` borrower, cr.email, 
 			gr.gender, sz.size, gi.`name`, ri.quantity
 		FROM request rq
@@ -581,7 +593,7 @@ CREATE PROCEDURE ViewRequestDetail(IN `RequestId` INT) READS SQL DATA
         JOIN gear_item gi ON ri.item_id = gi.id
         JOIN size sz ON gi.size_id = sz.id JOIN gender gr on gi.gender_id = gr.id
         WHERE rq.id = RequestId;
-DELIMITER $$
+END$$
 
 -- ================================================
 -- US 10

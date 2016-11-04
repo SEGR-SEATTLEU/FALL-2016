ee53375f10882e605141e0cd607e1a19a0a4f89b
michaelcheung2 <cheungm@uw.edu>
2016-11-04 16:30:15 -0700

    Mike's updates part 1

--- SQL/SQL_Database_Setup.sql
+++ SQL/SQL_Database_Setup.sql
@@ -606,6 +606,32 @@ BEGIN
 END$$
 
 -- ================================================
+-- US 9a
+-- ================================================
+DELIMITER ;
+DROP PROCEDURE IF EXISTS get_gear_inventory;
+DELIMITER $$
+CREATE PROCEDURE `get_gear_inventory`()
+BEGIN
+		SELECT  Inventory.id, Inventory.name, 
+			size.size,
+			IFNULL(Inventory.total_quantity - SUM(ReservedGears.quantity), Inventory.total_quantity) as QuantityAvailable	 
+		FROM ( 
+			SELECT a.id as GearID, SUM(b.quantity) as quantity
+			FROM gear_item as a
+			JOIN reserved_item as b
+				ON a.id = b.item_id
+                GROUP BY GearID) 
+			as ReservedGears
+		right JOIN gear_item as Inventory
+			ON Inventory.id = ReservedGears.GearID
+		JOIN size
+			ON size.id = Inventory.size_id
+		GROUP BY Inventory.id, Inventory.name, size.size;
+ END$$
+DELIMITER ;
+
+-- ================================================
 -- US 10
 -- ================================================
 DELIMITER ;            

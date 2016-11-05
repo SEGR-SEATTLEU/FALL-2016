eb09f96f78ebe4a7ddc0b09d2a4c00549c6d5624
KyleMcNutt <mcnuttkyle93@gmail.com>
2016-11-04 19:34:34 -0700

    Removing back button on more-gear-details page.

--- src/client/app/more-gear-details/more-gear-details.controller.js
+++ src/client/app/more-gear-details/more-gear-details.controller.js
@@ -13,17 +13,13 @@
     vm.id = $stateParams.gearID;
     vm.headerText = 'More Gear Details';
 
-
     activate();
 
     function activate() {
-      console.log("I could go for a milkshake"); 
-      /*vm.id = 10;*/
       moreGearDetails();
     }
 
     function moreGearDetails() {
-      console.log("The moreGearDetails function has been called successfully");
       WtaApi.getGearDetails(vm.id).then(function(gearItem) {
         vm.gearItem = gearItem;
         return vm.gearItem;
